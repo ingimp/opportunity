97d2b43fef53210cb169cf1a6644421f8bf02ca2
Luca Di Cesare <ing.dicesare@gmail.com>
2012-09-19 16:15:43 +0200

    Changes to the schema of db

--- certified-cv/src/sql/schema.sql
+++ certified-cv/src/sql/schema.sql
@@ -1,35 +1,37 @@
-create database if not exists certified_cv;
-use certified_cv;
-create table Address if not exists (
+create database if not exists certified_cv_db;
+
+use certified_cv_db;
+
+create table if not exists address (
 	id int not null primary key auto_increment,
 	street varchar(250) not null,
 	city varchar(50) not null,
 	state varchar(50) not null,
-	zipcode varchar(10) not null
-	ts_created timestamp default '0000-00-00 00:00:00';
-	ts_updated timestamp default now() on update now();
+	zipcode varchar(10) not null,
+	ts_updated timestamp,
+	ts_created timestamp
 	
-) TYPE=InnoDB;
+) Engine = InnoDB;
 
-create table if not exists Certied_CV (
+create table if not exists certified_cv (
 	id int not null primary key auto_increment,
 	cod varchar(16) not null,
 	index cod_cv_idx(cod),
-	ts_created timestamp default '0000-00-00 00:00:00';
-	ts_updated timestamp default now() on update now();
+	ts_updated timestamp,
+	ts_created timestamp
 	
-) TYPE = InnoDB;
+) Engine = InnoDB;
 
-create table if not exists Company_Card(
+create table if not exists company_card(
 	id int not null primary key auto_increment,
 	cod varchar(16) not null,
 	index cod_card_idx(cod),
-	ts_created timestamp default '0000-00-00 00:00:00';
-	ts_updated timestamp default now() on update now();
+	ts_updated timestamp,
+	ts_created timestamp
 	
-) TYPE = InnoDB;
+) Engine = InnoDB;
 
-create table if not exists Customer (
+create table if not exists customer (
 	id int not null primary key auto_increment,
 	username varchar(25) not null,
 	password varchar(15) not null,
@@ -38,33 +40,37 @@ create table if not exists Customer (
 	company_name varchar(250) default null,
 	p_iva varchar(11) default null,
 	email varchar(250) not null,
-	address int not null foreign key (address) references Address(id)
-	on delete cascade on update cascade;
+	address int not null,
+	foreign key (address) references address(id)
+	on update cascade on delete cascade,
 	index address_idx(address),
-	ts_created timestamp default '0000-00-00 00:00:00';
-	ts_updated timestamp default now() on update now();
+	ts_updated timestamp,
+	ts_created timestamp
 	
+) Engine = InnoDB;
 
-) TYPE=InnoDB;
-
-create table if not exists Customer_Certied_CV (
-	customer int not null foreign key (customer) references Customer(id)
+create table if not exists customer_certied_cV (
+	customer int not null,
+	certified_cv int not null,
+	foreign key (customer) references customer(id)
 	on delete cascade on update cascade,
-	certified_cv int not null foreign key (certief_cv) references Certified_CV(id)
-	on delete cascade on update cascade;
+	foreign key (certified_cv) references certified_cv(id)
+	on delete cascade on update cascade,
 	primary key (customer, certified_cv),
-	ts_created timestamp default '0000-00-00 00:00:00';
-	ts_updated timestamp default now() on update now();
+	ts_updated timestamp,
+	ts_created timestamp
 	
-) TYPE = InnoDB;
+) Engine = InnoDB;
 
-create table if not exists Customer_Company_Card (
-	customer int not null foreign key (customer) references Customer(id)
+create table if not exists customer_company_card (
+	customer int not null,
+	company_card int not null,
+	foreign key (customer) references customer(id)
 	on delete cascade on update cascade,
-	company_card int not null foreign key (Company_Card) references Company_Card(id)
+	foreign key (Company_Card) references company_card(id)
 	on delete cascade on update cascade,
 	primary key (customer, company_card),
-	ts_created timestamp default '0000-00-00 00:00:00';
-	ts_updated timestamp default now() on update now();
+	ts_updated timestamp,
+	ts_created timestamp
 	
-) TYPE = InnoDB;
+) Engine = InnoDB;
